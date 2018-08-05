5c0f60d8f81354f7ed01156012d5e5ec0253c20e
Jon Wexler <jwexman@gmail.com>
2018-08-04 23:30:56 -0400

    update callback url

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const passport = require('passport'),
 	passport.use(new GitHubStrategy({
 	    clientID: process.env.GITHUB_CLIENT_ID,
 	    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-	    callbackURL: process.env.DEV ? "http://localhost:3000/auth/github/callback" : "https://git-lister.herokuapp.com/auth/github/callback"
+	    callbackURL: process.env.DEV ? "http://localhost:3000/auth/github/callback" : "http://gitlister.com/auth/github/callback"
 	  },
 	  function(accessToken, refreshToken, profile, next) {
 
